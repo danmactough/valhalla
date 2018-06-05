4c4706c99b2792335455de132425897bc0370722
allierowan <allie.rowan@mapbox.com>
2018-06-05 16:00:07 -0400

    refactor

--- test/nodejs/route.test.js
+++ test/nodejs/route.test.js
@@ -1,12 +1,13 @@
 const test = require('tape');
-var Valhalla = require('../../');
+var valhalla = require('../../');
 
 var config = require('./fixtures/basic_config');
 
 test('route: can get a route in Hershey', function(assert) {
-  var valhalla = new Valhalla(JSON.stringify(config));
+  valhalla.configure(JSON.stringify(config));
+  var router = new valhalla.Actor();
   var hersheyRequest = '{"locations":[{"lat":40.546115,"lon":-76.385076,"type":"break"}, {"lat":40.544232,"lon":-76.385752,"type":"break"}],"costing":"auto"}';
-  var route = JSON.parse(valhalla.route(hersheyRequest));
+  var route = JSON.parse(router.route(hersheyRequest));
   assert.ok(route);
   assert.equal(route['trip']['status_message'], 'Found route between points', 'route was found');
   assert.equal(route['trip']['legs'].length, 1, 'Route has one leg');
@@ -15,17 +16,19 @@ test('route: can get a route in Hershey', function(assert) {
 });
 
 test('route: returns an error if no edges found', function(assert) {
-  var valhalla = new Valhalla(JSON.stringify(config));
+  valhalla.configure(JSON.stringify(config));
+  var router = new valhalla.Actor();
   var hersheyRequest = '{"locations":[{"lat":5,"lon":-76.385076,"type":"break"}, {"lat":40.544232,"lon":-76.385752,"type":"break"}],"costing":"auto"}';
-  assert.throws(() => { valhalla.route(hersheyRequest) }, /No suitable edges near location/, 'Throws correct error');
+  assert.throws(() => { router.route(hersheyRequest) }, /No suitable edges near location/, 'Throws correct error');
   assert.end();
 });
 
 test('route: returns an error if request format is wrong', function(assert) {
-  var valhalla = new Valhalla(JSON.stringify(config));
+  valhalla.configure(JSON.stringify(config));
+  var router = new valhalla.Actor();
   var hersheyRequest = '{"locations":[40.546115,-76.385076], [40.544232,"lon":-76.385752],"costing":"auto"}';
   // NOTE: we are not throwing a very useful error in this case - we should track this
   // down and throw something a little more descriptive
-  assert.throws(() => { valhalla.route(hersheyRequest) }, /std::exception/, 'Throws an error');
+  assert.throws(() => { router.route(hersheyRequest) }, /std::exception/, 'Throws an error');
   assert.end();
 });
